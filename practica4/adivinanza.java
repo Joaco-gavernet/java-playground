6478bfa610c9c1720330ddf62dd01b90c2d95f04
Joaquin Gavernet <70808627+Joaco-gavernet@users.noreply.github.com>
2023-04-22 17:35:34 -0300

    Update adivinanza.java
    new comments and fixed null issue in result.elemento(0) condition

--- practica4/adivinanza.java
+++ practica4/adivinanza.java
@@ -25,23 +25,24 @@ public class Adivinanza {
 	}
 	
 	public ListaGenericaEnlazada<ListaGenericaEnlazada<String>> secuenciaConMasPreguntasVersion2(ArbolBinario<String> abinario) {
-		
+		// CREO LISTAS DE LISTAS, RESULTADO (PARA RETORNAR) Y AUXILIAR (PARA ANALIZAR SITUACION DEL NODO ACTUAL)
 		ListaGenericaEnlazada<ListaGenericaEnlazada<String>> result = new ListaGenericaEnlazada<ListaGenericaEnlazada<String>>();
 		ListaGenericaEnlazada<ListaGenericaEnlazada<String>> aux = new ListaGenericaEnlazada<ListaGenericaEnlazada<String>>();
 		
 		if (abinario.esHoja()) {
-			
+			// BASE CASE ///////////////
 			ListaGenericaEnlazada<String> nueva = new ListaGenericaEnlazada<String>();
 			nueva.agregarFinal(abinario.getDato());
 			result.agregarFinal(nueva);
 			
 		} else {
-			
+			// PROCESO HIJO IZQUIERDO ///////////
 			if (abinario.tieneHijoIzquierdo()) {
-				aux = secuenciaConMasPreguntasVersion2(abinario.getHijoIzquierdo());
-				System.out.println(aux.elemento(0).tamanio());
+				aux = secuenciaConMasPreguntasVersion2(abinario.getHijoIzquierdo()); // RECURSION RECUPERADA EN VARIABLE aux
 				
-				if ((aux.elemento(0)).tamanio() > (result.elemento(0)).tamanio()) {
+				// SI EL RESULTADO ES NULO O EL CAMINO ACTUAL ES MAYOR QUE LOS ALMACENADOS EN RESULTADO ENTONCES 
+				// VACIO LA LISTA RESULTADO Y AGREGO EL CAMINO ACTUAL COMO NUEVA SOLUCION DE MAYOR TAMANIO
+				if ((result.elemento(0) == null) || (aux.elemento(0).tamanio() > result.elemento(0).tamanio())) {
 					while (!result.esVacia()) result.eliminarEn(0);
 					aux.comenzar();
 					while (!aux.fin()) result.agregarFinal(aux.proximo());
@@ -51,10 +52,11 @@ public class Adivinanza {
 				}
 			}
 			
+			// PROCESO HIJO DERECHO ////////////
 			if (abinario.tieneHijoDerecho()) {
 				aux = secuenciaConMasPreguntasVersion2(abinario.getHijoDerecho());
 				
-				if (aux.elemento(0).tamanio() > result.elemento(0).tamanio()) {
+				if ((result.elemento(0) == null) || (aux.elemento(0).tamanio() > result.elemento(0).tamanio())) {
 					while (!result.esVacia()) result.eliminarEn(0);
 					aux.comenzar();
 					while (!aux.fin()) result.agregarFinal(aux.proximo());
@@ -64,6 +66,7 @@ public class Adivinanza {
 				}
 			}
 			
+			// AGREGO DATO A TODAS LAS LISTAS RESULTADOS ////////////////
 			result.comenzar();
 			while (!result.fin()) result.proximo().agregarFinal(abinario.getDato());
 		}
